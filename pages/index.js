2cdd7059cd311a88eedc3497c6f18975b0d8a87c
Maximilian David <med32095@yahoo.com>
2022-11-03 22:25:11 -0400

    fetched data successfully, unlike my spelling

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,35 @@
+import clientPromise from "../lib/mongodb";
 
+export default function Home({ times }) {
+    return (
+        <div>
+            <h1>All Times</h1>
+            <ul>
+                {times.map((times) => (
+                    <li key={times._id}>
+                        <h2>{times.time.$numberDecimal}</h2>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
 
-export default function Home() {
-  return (
-    <div>
-      hello
-    </div>
-  )
+export async function getServerSideProps() {
+    try {
+        const client = await clientPromise;
+        const db = client.db("cuber");
+
+        const times = await db
+            .collection("times")
+            .find({})
+            .limit(20)
+            .toArray();
+
+        return {
+            props: { times: JSON.parse(JSON.stringify(times)) },
+        };
+    } catch (e) {
+        console.error(e);
+    }
 }
