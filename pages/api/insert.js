833be9da2f9e6ff8e0f0f44f100fff14a28c622c
Maximilian David <med32095@yahoo.com>
2022-11-04 21:53:11 -0400

    posts data. need to refresh

---
+++ pages/api/insert.js
@@ -0,0 +1,22 @@
+import clientPromise from "../../lib/mongodb";
+
+export default async (req, res) => {
+    try {
+        // connect to the database
+        const client = await clientPromise;
+        const db = client.db("cuber");
+        // add the time
+        await db.collection('times').insertOne(JSON.parse(req.body));
+        // return a message
+        return res.json({
+            message: 'time added successfully',
+            success: true,
+        });
+    } catch (error) {
+        // return an error
+        return res.json({
+            message: new Error(error).message,
+            success: false,
+        });
+    }
+};
--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,63 @@
 import clientPromise from "../lib/mongodb.js"
+import { useState } from "react";
 
 export default function Home({ times }) {
+    const [time, setTime] = useState(null)
+    const [error, setError] = useState('');
+    const [message, setMessage] = useState('');
+
+     const insertTime = async (e) => {
+        e.preventDefault();
+
+        // reset error and message
+        setError('');
+        setMessage('');
+
+        // fields check. form validation???????
+        if (!time) return setError('All fields are required');
+
+        // entry structure
+        let entry = {
+            time: parseFloat(time),
+            //createdAt: new Date().toISOString(),
+        };
+        // save the time
+        let response = await fetch('/api/insert', {
+            method: 'POST',
+            body: JSON.stringify(entry),
+        });
+
+        // get the data
+        let data = await response.json();
+
+        if (data.success) {
+            // reset the fields
+            setTime('');
+            // set the message
+            return setMessage(data.message);
+        } else {
+            // set the error
+            return setError(data.message);
+        }
+        };
+
+
     return (
         <div className='flex flex-col items-center gap-4 mt-3'>
             <h1 className='text-6xl text-slate-800'>All Times</h1>
             <div>
-                <form className='flex flex-col items-center gap-2'>
-                    <input className='rounded'/>
+                <form onSubmit={insertTime} className='flex flex-col items-center gap-2'>
+                    <input className='rounded' onChange={(e) => setTime(e.target.value)}/>
+                    <div>{time}</div>
+                    <div>{message}</div>
+                    <div>{error}</div>
                     <button type="submit" className='bg-slate-600 text-white rounded py-1 px-2'>SUBMIT</button>
                 </form>
             </div>
             <ul className='border-2 border-slate-600 rounded-md p-1'>
                 {times.map((times) => (
                     <li key={times._id}>
-                        <h2>{times.time.$numberDecimal}</h2>
+                        <h2>{times.time}</h2>
                     </li>
                 ))}
             </ul>
