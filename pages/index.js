139b6eff836fdd5a5ea1d2dba474557ca6ddfeed
Maximilian David <med32095@yahoo.com>
2022-11-04 21:05:18 -0400

    form html css

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,14 @@ import clientPromise from "../lib/mongodb.js"
 export default function Home({ times }) {
     return (
         <div className='flex flex-col items-center gap-4 mt-3'>
-            <h1 className='text-3xl text-white'>All Times</h1>
-            <ul className='border-2 border-slate-800 rounded-md p-1'>
+            <h1 className='text-6xl text-slate-800'>All Times</h1>
+            <div>
+                <form className='flex flex-col items-center gap-2'>
+                    <input className='rounded'/>
+                    <button type="submit" className='bg-slate-600 text-white rounded py-1 px-2'>SUBMIT</button>
+                </form>
+            </div>
+            <ul className='border-2 border-slate-600 rounded-md p-1'>
                 {times.map((times) => (
                     <li key={times._id}>
                         <h2>{times.time.$numberDecimal}</h2>
