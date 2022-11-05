532bbf2cb397f3951ef364415a7e83f3e216faee
= <med32095@yahoo.com>
2022-11-05 14:55:14 -0400

    stopwatch css

--- components/stopwatch.js
+++ components/stopwatch.js
@@ -1,7 +1,24 @@
-
+import { useState } from "react"
 
 export default function Stopwatch() {
+    const [time, setTime] = useState(0);
+
+    let min = "0"
+    let sec = "00"
+    let ms = "000"
+
+    function startTimer() {
+
+    }
+
     return (
-        <div>... insert stopwatch here ...</div>
+        <div className='flex flex-col gap-4 justify-between items-center border-4 border-slate-600 rounded-md p-4'>
+            <div className='text-5xl text-slate-800'>
+                {min}:{sec}.{ms}
+            </div>
+            <button className='bg-slate-600 rounded px-2 text-white w-full hover:bg-slate-800'>
+                START
+            </button>
+        </div>
     )
 }
