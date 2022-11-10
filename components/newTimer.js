37467238559ada8d9b5d4f4baadaae0127ca5146
Maximilian David <med32095@yahoo.com>
2022-11-09 20:55:56 -0500

    rq fetch is implemented

---
+++ components/newTimer.js
@@ -0,0 +1,38 @@
+import { useTimes } from "../hooks/useTimes";
+import Chart from "./chart";
+
+export default function NewTimer({ session }) {
+
+    const { data, isLoading, isFetching } = useTimes(session.user.id)
+
+    if (session) {
+        if (isLoading) return <div>Loading</div>
+        if (isFetching) return <div>fetching</div> //idk if necessary?
+    
+        return (
+            <section>
+                <Chart times={data}/>
+                <ul>
+                    {data?.map((time) => (
+                    <li key={time._id}>
+                        {time.prettyTime} : {time.userID} : {time.createdAt}
+                    </li>
+                    ))}
+                </ul>
+                {/* {postCount <= 90 && (
+                    <button
+                    onClick={() => setPostCount(postCount + 10)}
+                    disabled={isFetching}
+                    >
+                    {isFetching ? 'Loading...' : 'Show More'}
+                    </button>
+                )} */}
+            </section>
+        )
+    }
+    return (
+        <div>
+            no session
+        </div>
+    )
+}
