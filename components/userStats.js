87e2d1ae45820629f607686801d92a5046563000
= <med32095@yahoo.com>
2022-11-12 14:08:32 -0500

    stats

--- components/userStats.js
+++ components/userStats.js
@@ -3,6 +3,36 @@ import { useTimes } from "../hooks/useTimes.js";
 
 import { useMutation, useQueryClient } from "react-query";
 
+function statistics(data) {
+    let times = []
+    data.map(time => {
+        times.push(time.time)
+    })
+    return times
+}
+
+function pretify(time) {
+    let min = Math.floor(time/60000)
+    let sec = Math.floor((time % 60000)/1000)
+    let ms = time % 1000
+
+    let _min = String(min)
+    let _sec = String(sec)
+    let _ms = String(ms)
+
+    if (sec <= 9) {
+        _sec = `0${sec}`
+    }
+    if (ms <= 99) {
+        if (ms <= 9) {
+            _ms = `00${ms}`
+        } else {
+            _ms = `0${ms}`
+        }
+    }
+    
+    return `${_min}:${_sec}.${_ms}`
+}
 
 export default function UserStats({ session }) {
     const { data, isLoading, isFetching } = useTimes(session.user.id);
@@ -39,7 +69,18 @@ export default function UserStats({ session }) {
                         </tr>
                     ))}
                 </tbody>
-            </table> 
+            </table>
+            <div className='font-mono'>
+                <div>
+                    Count : {data.length}
+                </div>
+                <div>
+                    Min : {pretify(Math.min(...(statistics(data))))}
+                </div>
+                <div>
+                    Min : {pretify(Math.max(...(statistics(data))))}
+                </div>
+            </div>
             <div className='py-5'>
                 <Chart times={data}/>
             </div>
